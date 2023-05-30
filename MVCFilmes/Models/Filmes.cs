31499128dde901fde3283551286a595ac68d4167
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-05-30 16:38:23 -0300

    Controllers e views

---
+++ MVCFilmes/Models/Filmes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCFilmes.Models
+{
+    public class Filmes
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+        public string? Genero { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
