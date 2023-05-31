0f3e6661e347e5bda0ebc322997f74436189e9f4
Rafael Marques <rafaelmarques.rm86@gmail.com>
2023-05-31 12:04:18 -0300

    Adicionando data anotation

--- MVCFilmes/Models/Filmes.cs
+++ MVCFilmes/Models/Filmes.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCFilmes.Models
 {
@@ -7,9 +8,13 @@ namespace MVCFilmes.Models
         public int Id { get; set; }
         public string? Titulo { get; set; }
 
+        [Display(Name ="Data de Lançamento")]
         [DataType(DataType.Date)]
         public DateTime DataLancamento { get; set; }
         public string? Genero { get; set; }
+
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Preco { get; set; }
     }
 }
